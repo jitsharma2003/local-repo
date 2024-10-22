98ecaf6b33e0ae587b59e5bf729346e2aebc3d08
jitsharma2003 <jit@jits-MacBook-Air.local>
2024-10-22 19:26:17 +0530

    two file added

---
+++ new.py
@@ -0,0 +1,9 @@
+colour = input("enter a colour :")
+if(colour == "green"):
+    print("gooo")
+elif(colour == "yellow"):
+    print("check and go")
+elif(colour == "red"):
+    print("stoppp")
+else:
+    print("wrong colout")
